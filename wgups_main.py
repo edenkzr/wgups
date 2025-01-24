95a7ba92e870578a88a82d5feb41441dfd9e1871
deegz <cuadd.eden@gmail.com>
2025-01-24 15:32:07 -0500

    csv and package class

--- wgups_main.py
+++ wgups_main.py
@@ -1,3 +1,6 @@
+import csv
+
+
 #class for hash table with collision handling capabilities via chaining
 class ChainingHashTable:
 
@@ -53,6 +56,42 @@ class ChainingHashTable:
             if kv[0] == key:
                 bucket_list.remove(kv[0], kv[1])
 
+class Package:
+    def __init__(self, package_ID, address, city, state, zip, delivery_deadline, weight):
+        self.package_ID = package_ID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.delivery_deadline = delivery_deadline
+        self.weight = weight
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.package_ID, self.address, self.city, self.state, self.zip, self.delivery_deadline, self.weight)
+
+def loadPackageData(fileName):
+    with open(fileName) as Packages:
+        packageData = csv.reader(Packages, delimiter=',')
+        next(packageData)
+        for package in packageData:
+            packageID = int(package[0])
+            packageAddress = package[1]
+            packageCity = package[2]
+            packageState = package[3]
+            packageZip = package[4]
+            packageDeliveryDeadline = package[5]
+            packageWeight = package[6]
+
+            p = Package(packageID, packageAddress, packageCity, packageState, packageZip,
+                        packageDeliveryDeadline, packageWeight)
+
+            print(p)
+
+            h.insert(packageID, p)
+
 
 h = ChainingHashTable()
-h.insert
+loadPackageData("wgups_packages.csv")
+
+for i in range(len(h.table)):
+    print("Key: {} and Package: {}".format(i+1, h.search(i+1)))
